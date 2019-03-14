49350ec91d8aa453d166536a59427f64d14c7f6e
VictorShedko <48150306+VictorShedko@users.noreply.github.com>
2019-03-14 16:38:00 +0300

    new controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,36 @@
 package sample;
-import java.util.Random;
+import sample.logic.Interface;
+import sample.logic.Functors.*;
+
+import java.util.*;
+
 public class Controller {
 
-    public static int getRand(int Max){
-        Random r = new Random();
-        return r.nextInt(Max);
+    Map<String,Interface> mymap=new HashMap<String, Interface>();
+
+    Controller(){
+        Randomizer randomaizerObject=new Randomizer();
+        Subtractor subtractorObject=new Subtractor();
+        Adder adderObject=new Adder();
+        this.mymap.put("add",adderObject);
+        this.mymap.put("sub",subtractorObject);
+        this.mymap.put("random",randomaizerObject);
+
     }
+    public Interface getObectToDo(String inputString){
+
+        return mymap.get(inputString);
+
+    }
+    public int analyze(String inputMessege,Integer a,Integer b){
+
+
+        Interface objectToDo= mymap.get(inputMessege);
+        return objectToDo.doSomething(a,b);
+
+    }
+
+
+
 
 }
